729c6333569b6f8334ab23ad68e56ee1bbb12fc0
Mahpud || Nixx <bamat7010@gmail.com>
2026-01-31 10:58:28 +0700

    Create ytmp3.js

---
+++ src/api/search/ytmp3.js
@@ -0,0 +1,32 @@
+
+module.exports = function(app) {
+    const ytdl = require('ytdl-core');
+    app.get('/search/ytmp3', async (req, res) => {
+        const { url } = req.query;
+        if (!url) {
+            return res.status(400).json({ status: false, error: 'URL is required' });
+        }
+
+        try {
+            if (!ytdl.validateURL(url)) {
+                return res.status(400).json({ status: false, error: 'Invalid YouTube URL' });
+            }
+
+            const info = await ytdl.getInfo(url);
+            const audioFormat = ytdl.chooseFormat(info.formats, { quality: 'highestaudio', filter: 'audioonly' });
+
+            res.status(200).json({                
+                status: true,
+                result: {
+                    imgurl: info.videoDetails.thumbnails[info.videoDetails.thumbnails.length - 1].url,
+                    audiourl: audioFormat.url,
+                    title: info.videoDetails.title,
+                    duration: info.videoDetails.lengthSeconds,
+                    author: info.videoDetails.author.name
+                }
+            });
+        } catch (error) {
+            res.status(500).json({ status: false, error: error.message });
+        }
+    });
+};
